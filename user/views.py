290c7229db2a6ae8e6ac43ab9defc5e22e1d154b
Abdoulaye SARR <dadscientific@hotmail.com>
2020-11-08 15:22:16 +0100

    Ajout d'un gabarit parent base.html pour éviter les copie-coller dans les templates

--- user/views.py
+++ user/views.py
@@ -1,5 +1,4 @@
 from user.forms import UserForm
-from django.http import HttpResponseRedirect
 from user.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
@@ -8,11 +7,12 @@ def connexion(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-        	email = form.cleaned_data['email']
-        	password = make_password(form.cleaned_data['password'])
- 
-	       	User.objects.create(email=email,password=password)
-        	return redirect('/view')	        
+            email = form.cleaned_data['email']
+            password = make_password(form.cleaned_data['password'])
+            user = User.objects.filter(email=email)
+            if not user.exists():
+                User.objects.create(email=email,password=password)
+        return redirect('/view')	
     else:
         form = UserForm() 
     return render(request, 'index.html', {'form': form}) 
