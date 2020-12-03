10728bb811595c34e3e2e00e27f7a02e936c1018
Abdoulaye SARR <dadscientific@hotmail.com>
2020-12-03 13:36:15 +0100

    Revue de code, version finale

--- user/forms.py
+++ user/forms.py
@@ -3,9 +3,10 @@ from user.models import User
 # Create your models here.
 
 class UserForm(forms.ModelForm):
-	email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-	password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
-
 	class Meta:
 		model = User
 		fields = '__all__'
+		widgets = {
+			'email': forms.EmailInput(attrs={'class': 'form-control'}),
+			'password': forms.PasswordInput(attrs={'class' : 'form-control'})
+		}
